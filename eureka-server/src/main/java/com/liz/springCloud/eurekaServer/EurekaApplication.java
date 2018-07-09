4a5818bd96be0ed4c74d60922705588c20399561
lizhou828 <lizhou828@126.com>
2018-07-09 11:02:01 +0800

    SpringCloud【一】服务的注册与发现（Eureka）:https://blog.csdn.net/forezp/article/details/69696915

---
+++ eureka-server/src/main/java/com/liz/springCloud/eurekaServer/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.liz.springCloud.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//只需要一个注解@EnableEurekaServer，这个注解需要在springboot工程的启动application类上加@EnableEurekaServer,就能启动一个服务注册中心
+//eureka server 是有界面的，启动工程,打开浏览器访问：http://localhost:8761 ,
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
